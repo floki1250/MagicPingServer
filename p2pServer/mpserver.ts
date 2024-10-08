78c7d64ce7d5f8e00b52d89aaee829f12a6e77a0
Adem <37814393+floki1250@users.noreply.github.com>
2024-10-08 17:39:31 +0100

    Fix

--- p2pServer/mpserver.ts
+++ p2pServer/mpserver.ts
@@ -1,15 +1,13 @@
 import { ExpressPeerServer } from "peer";
 import express from "express";
-const customGenerationFunction = () =>
-  (Math.random().toString(36) + "0000000000000000000").substr(2, 16);
+
 const app = express();
-const server = app.listen(3000);
+const server = app.listen(443);
 
 // PeerJS server middleware
 const peerServer = ExpressPeerServer(server, {
   path: "/",
   allow_discovery: true,
-  generateClientId: customGenerationFunction,
 });
 
 app.use("/peerjs", peerServer);
